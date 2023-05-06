24195da485ebace7f2584b2f3f93b7d780f34403
jinZheSheng <2803290705@qq.com>
2023-05-06 18:03:47 +0800

    goodList created

---
+++ 8. 小程序/uniappShop/uni-shop/components/good-item/good-item.vue
@@ -0,0 +1,50 @@
+<template>
+  <view class="good-item-wrap">
+    <view class="good-item-left">
+      <image :src="good.goods_small_logo || defaultURL"></image>
+    </view>
+    <view class="good-item-right">
+        <view class="good-item-name">
+          {{good.goods_name}}
+        </view>
+        <view class="good-item-price">
+          $ {{good.goods_price}}
+        </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"good-item",
+    props: {
+      good: {
+        type: Object,
+        default: {}
+      }
+    },
+    data() {
+      return {
+        defaultURL: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      };
+    }
+  }
+</script>
+
+<style lang="stylus">
+  .good-item-wrap
+    display: flex
+    padding: 10px 6px
+    .good-item-left
+      image
+        width: 100px
+        height: 100px
+  .good-item-right
+    display: flex
+    flex-direction: column
+    justify-content: space-between
+    .good-item-name
+      font-size: 14px
+    .good-item-price
+      color: #c00000
+</style>
--- 8. 小程序/uniappShop/uni-shop/subpkg/goods_list/goods_list.vue
+++ 8. 小程序/uniappShop/uni-shop/subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
   <view>
-    goods list
+    <!-- goods list -->
+    
+    <view class="good-item" v-for="(item,idx) in goodsList" :key="idx" @click="naviToDetail(item)">
+      <good-item :good="item"></good-item>
+    </view>
   </view>
 </template>
 
@@ -8,12 +12,61 @@
   export default {
     data() {
       return {
-        
+        paramObj: {
+          query: "",
+          cid: "",
+          pagenum: 1,
+          pagesize: 10
+        },
+        goodsList: [],
+        total: 0,
+        isLoading: false
       };
+    },
+    onLoad(options) {
+      if (options.query) this.paramObj.query = options.query
+      if (options.cid) this.paramObj.cid = options.cid
+      this.getGoodList()
+    },
+    onReachBottom() {
+      //加载新数据
+      if(this.paramObj.pagenum*this.paramObj.pagesize >= this.total) return uni.$showMessage("没有了捏~~")
+      if(this.isLoading) return
+      // console.log("new data")
+      this.paramObj.pagenum++
+      this.getGoodList()
+    },
+    onPullDownRefresh() {
+      //重置数据
+      this.goodsList = []
+      this.total = 0
+      this.isLoading = false
+      this.paramObj.pagenum = 1
+      this.getGoodList(() => uni.stopPullDownRefresh())
+    },
+    methods: {
+      async getGoodList(cb) {
+        this.isLoading = true
+        let {data: res} = await uni.$http.get("/api/public/v1/goods/search",this.paramObj)
+        this.isLoading = false
+        cb && cb()
+        if(res.meta.status !== 200) return uni.$showMessage()
+        this.goodsList = [...this.goodsList,...res.message.goods]
+        this.total = res.message.total
+      },
+      naviToDetail(item) {
+        uni.navigateTo({
+          url: `/subpkg/goods_detail/goods_detail?goods_id=${item.goods_id}`
+        })
+      }
     }
   }
 </script>
 
 <style lang="stylus">
-
+  .good-item
+    padding: 6px
+    margin: 4px 8px
+    margin-bottom: 4px
+    border-bottom: 1rpx solid #bebebe
 </style>
